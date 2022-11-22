7b67f5f95798a21ab7dd1c95aea2bad30b6726b0
krishna-saini <krishnapraanjal10@gmail.com>
2022-11-22 23:30:16 +0530

    updated routes file

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const taskRouter = express.Router();
-const { getTodos, addTodo, getTasks, addTask} = require("../controllers/todoContoller");
+const { getTodos, addTodo,updateTodo,deleteTodo, getTasks, addTask, updateTask, deleteTask} = require("../controllers/todoContoller");
 
 
-router.route("/").get(getTodos).post(addTodo);
+router.route("/").get(getTodos).post(addTodo).patch(updateTodo).delete(deleteTodo);
 
 // router.param("todoId", (req, res, next, val) => {
 //   console.log(val);
 //   next();
 // });
-router.route("/:todoId").get(getTasks).post(addTask)
+router.route("/:todoId").get(getTasks).post(addTask).patch(updateTask).delete(deleteTask)
 
 module.exports = { router, taskRouter };
